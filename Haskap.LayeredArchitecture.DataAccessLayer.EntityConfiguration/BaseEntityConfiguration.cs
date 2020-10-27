70eb64e156669704ce86070ceba6449949efbdf5
Hasan Kaplan <heisenbugh@gmail.com>
2020-10-27 15:11:50 +0300

    BaseEntityConfiguration yeniden EntityConfiguration projesi içerisinde eklendi.

---
+++ Haskap.LayeredArchitecture.DataAccessLayer.EntityConfiguration/BaseEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Haskap.LayeredArchitecture.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Haskap.LayeredArchitecture.DataAccessLayer.EntityConfiguration
+{
+    public class BaseEntityConfiguration<TEntity, TId> : IEntityTypeConfiguration<TEntity>
+        where TEntity : BaseEntity<TId>
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            if (typeof(ISoftDeletable).IsAssignableFrom(typeof(TEntity)))
+            {
+                builder.HasQueryFilter(x => (x as ISoftDeletable).IsDeleted == false);
+            }
+        }
+    }
+}
